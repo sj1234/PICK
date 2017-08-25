810478892f11a6d5a9384fcdc94758bdeeeaca79
sj1234 <cu9997@naver.com>
2017-08-25 16:56:43 +0900

    FAQ, Item Activity 수정중2

--- app/src/main/java/com/example/sjeong/pick/ItemActivity.java
+++ app/src/main/java/com/example/sjeong/pick/ItemActivity.java
@@ -1,9 +1,14 @@
 package com.example.sjeong.pick;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -19,6 +24,8 @@ public class ItemActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar)findViewById(R.id.item_toolbar);
         toolbar.setTitle("");
+        //toolbar.setBackground(new ColorDrawable(Color.parseColor("#00ff0000")));
+        toolbar.getBackground().setAlpha(0);
         setSupportActionBar(toolbar);
 
         // 뒤로가기 버튼
@@ -33,22 +40,29 @@ public class ItemActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
-        getMenuInflater().inflate(R.menu.menu_faq, menu);
+        getMenuInflater().inflate(R.menu.menu_item, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
         int id = item.getItemId();
+
+        Drawable full = ContextCompat.getDrawable(this, R.drawable.full_star);
+        Drawable empty = ContextCompat.getDrawable(this, R.drawable.empty_star);
+        Bitmap bitmap1 = ((BitmapDrawable)full).getBitmap();
+
         switch(id){
             case android.R.id.home:
                 finish();
                 break;
             case R.id.star:
-                if(item.getIcon()==getDrawable(R.drawable.up))
-                    item.setIcon(getDrawable(R.drawable.down));
+                Log.i("Test", "Click star button");
+                Bitmap bitmap3 = ((BitmapDrawable)(item.getIcon())).getBitmap();
+                if(bitmap3.equals(bitmap1))
+                    item.setIcon(empty);
                 else
-                    item.setIcon(getDrawable(R.drawable.up));
+                    item.setIcon(full);
                 break;
 
         }
